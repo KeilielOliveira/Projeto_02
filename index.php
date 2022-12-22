e35cf86413120359675846bfa7edb5dbbc78e159
Keiliel Oliveira <keilieloliveira.0@gmail.com>
2022-12-22 11:11:35 -0300

    22/12/2022 v1
    Projeto sem os reajustes.

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto 1</title>
+</head>
+<body>
+
+
+<nav class="menu-desktop">
+    <div class="container">
+        <div class="logo">
+            <h2>Logomarca</h2>
+        </div><!--logo-->
+        <ul class="ul-menu-desktop">
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Contato</a></li>
+        </ul><!--ul-menu-desktop-->  
+        <div class="menu-icon"></div> 
+        <ul class="ul-menu-mobile">
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Contato</a></li>
+        </ul><!--ul-menu-mobile--> 
+    </div>
+</nav><!--menu-desktop-->
+<div class="clear"></div>
+
+<section class="banner">
+        <div class="bg-cover">
+            <div class="container">
+                <div class="content-banner">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio vitae modi ullam officiis doloremque expedita</p>
+                    <div class="form-banner">
+                        <form action="" class="banner">
+                            <input type="email" name="email" id="" required placeholder="E-mail">
+                            <input type="submit" value="Enviar">
+                        </form><!--banner--> 
+                    </div><!--form-banner--> 
+                </div><!--content-banner--> 
+            </div>
+        </div><!--bg-cover--> 
+</section><!--banner--> 
+
+<section class="sobre">
+    <div class="container">
+        <div class="content-sobre-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non accusamus repellendus voluptatibus consequatur eaque labore alias vero id accusantium magni nemo atque, cum eius. Ex architecto cumque expedita odio facilis.</p>
+        </div><!--content-sobre-description--> 
+        <div class="content-sobre">
+            <div class="box-1">    
+                <div class="text-content-box-1">
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsam ipsum sapiente delectus impedit itaque expedita labore optio. Ullam ut accusantium est expedita tempore velit sit beatae numquam! Delectus, libero.</p>
+                </div><!--text-content-box-1--> 
+                <div class="box-1-img"><img class="box-1-img" src="images/box_1.jpg" alt="">
+            </div><!--box-1-img-->     
+            </div><!--box-1--> 
+            <div class="clear"></div>
+            <div class="box-2">
+                <div class="box-2-img">               
+                </div><!--box-2-img--> 
+                <div class="depoimento">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis similique incidunt quidem cumque nobis qui voluptatem repudiandae deserunt nisi id voluptatibus aspernatur ipsum placeat, labore consectetur, quae ipsam amet totam?</p>
+                    <h3>Kennedy Gomes</h3>
+                </div><!--depoimento--> 
+            </div><!--box-2--> 
+        </div><!--content-sobre-->
+        
+        
+    </div>
+</section><!--sobre--> 
+
+<section class="servicos">
+    <div class="container">
+        <h3>Como podemos ajudar</h3>
+        <div class="icon-arrow"></div>
+        <div class="section-servicos">
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            <div class="box-servicos">
+                <div class="icon-servicos"></div>
+                <h3>lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod in nam ab nihil, quo veritatis nemo vero nostrum quasi atque consectetur? Hic voluptate eos nemo deserunt, tempora numquam repudiandae?</p>
+                <button><a href="">Ver mais!</a></button>
+            </div><!--box-servicos-->
+            
+        </div><!--section-servicos--> 
+    </div>
+</section><!--servicos--> 
+
+<section class="contato">
+    <div class="container">
+        <h2>Entre em contato!</h2>
+        <form action="">
+            <input type="text" name="Nome" id="" placeholder="Nome">
+            <input type="email" name="email" id="" placeholder="Email">
+            <textarea name="assunto" id="" cols="30" rows="10"></textarea>
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+</section><!--contato--> 
+
+<footer>
+    <div class="container">
+        <div class="content-footer"><h3>todos os direitos reservados</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptate doloremque iste cupiditate saepe obcaecati ipsa ipsam veritatis impedit voluptatibus ex, perferendis, suscipit exercitationem nemo velit autem odit nihil aliquam?</p>
+        </div><!--content-footer--> 
+        
+    </div>
+</footer>
+
+
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/javascript.js"></script>
+
+</body>
+</html>
