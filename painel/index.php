96595da2d7b36676af1a6589d30fd52587e7a805
Keiliel Oliveira <keilieloliveira.0@gmail.com>
2023-01-03 11:04:38 -0300

    03/01/2023 v1
    HTML e CSS do painel de controle.

---
+++ painel/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9513630920.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Controle</title>
+</head>
+<body>
+<div class="flex">
+    <section class="controller">
+        <div class="header-top">
+            <h2>Painel</h2>
+        </div>
+        <div class="options">
+            <div class="op-sec">
+                <h2>Header</h2>
+                <ul>
+                    <li><a href="">Adicionar item ao menu</a></li>
+                    <li><a href="">Remover item do menu</a></li>
+                    <li><a href="">Alterar item do menu</a></li>
+                </ul>
+            </div><!--op-sec--> 
+            <div class="op-sec">
+                <h2>Banner</h2>
+                <ul>
+                    <li><a href="">Modificar banner</a></li>
+                </ul>
+            </div>
+            <div class="op-sec">
+                <h2>Seção Sobre</h2>
+                <ul>
+                    <li><a href="">Alterar textos da seção sobre</a></li>
+                    <li><a href="">Alterar imagens da seção sobre</a></li>
+                    <li><a href="">Modificar seção de serviços</a></li>
+                </ul>
+            </div>
+            <div class="op-sec">
+                <h2>Seção Contato</h2>
+                <ul>
+                    <li><a href="">Alterar formulario de contato</a></li>
+                </ul>
+            </div>
+            <div class="op-sec">
+                <h2>Footer</h2>
+                <ul>
+                    <li><a href="">Alterar footer</a></li>
+                </ul>
+            </div>
+        </div><!--options--> 
+    </section>
+
+    <header class="haeader-painel">
+        <i class="fa-solid fa-bars icon"></i>
+        <p>contato.sac@gmail.com</p>
+    </header>
+</div><!--Display flex--> 
+    
+</body>
+</html>
